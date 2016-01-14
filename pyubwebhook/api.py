dfd2f64760873d5d7c4ca8c73939b72051be994a
Mathieu Mitchell <mmitchell@iweb.com>
2016-01-14 14:54:45 -0500

    Python3 support

--- pyubwebhook/api.py
+++ pyubwebhook/api.py
@@ -1,5 +1,6 @@
 import json
-from flask import request
+from flask import request, current_app
+
 
 class WebhookApi(object):
     def __init__(self, app, router):
@@ -11,11 +12,18 @@ class WebhookApi(object):
 
 
     def list_implemented_methods(self, module_name):
-        return json.dumps({
+        return json_response({
             'implemented_methods': self.router.list_implemented_methods(module_name)
-        })
+        }, 200)
 
     def handle_remote_invocation(self, module_name):
-        data = json.loads(request.data)
+        data = request.get_json()
         output = self.router.invoke_method(module_name=module_name, **data)
-        return json.dumps(output)
+        return json_response(output, 200)
+
+def json_response(data, code):
+    json_data = json.dumps(data, indent=None)
+    response = current_app.response_class(json_data, mimetype='application/json; charset=UTF-8')
+    response.status_code = code
+
+    return response
--- tests/test_api.py
+++ tests/test_api.py
@@ -17,19 +17,21 @@ class ApiTest(unittest.TestCase):
     def test_list_implemented_methods(self):
         self.router.list_implemented_methods.return_value = ['abcd', 'efgh']
 
-        implemented_methods = self.api_client.get('/module1/').data
+        output = self.api_client.get('/module1/')
         self.router.list_implemented_methods.assert_called_with('module1')
 
-        assert_that(implemented_methods, is_(json.dumps({
+        assert_that(json.loads(output.data.decode(output.charset)), is_({
             "implemented_methods": [
                 "abcd",
                 "efgh"
             ]
-        })))
+        }))
 
     def test_execute_method_returns_string(self):
         self.router.invoke_method.return_value = 'simple string'
-        output = self.api_client.post('/module2/', data=json.dumps(
+        output = self.api_client.post('/module2/',
+                                      headers={'Content-Type': 'application/json'},
+                                      data=json.dumps(
             {
                 "method": "remote_method",
                 "params": [],
@@ -41,11 +43,13 @@ class ApiTest(unittest.TestCase):
         ))
 
         self.router.invoke_method.assert_called_with(module_name='module2', method='remote_method', params=[], env={'variable1': 'value1'}, callback={})
-        assert_that(output.data, is_(json.dumps('simple string')))
+        assert_that(json.loads(output.data.decode(output.charset)), is_('simple string'))
 
     def test_execute_method_returns_list(self):
         self.router.invoke_method.return_value = ['a', 'b', 'c']
-        output = self.api_client.post('/module2/', data=json.dumps(
+        output = self.api_client.post('/module2/',
+                                      headers={'Content-Type': 'application/json'},
+                                      data=json.dumps(
             {
                 "method": "remote_method",
                 "params": [],
@@ -57,5 +61,5 @@ class ApiTest(unittest.TestCase):
         ))
 
         self.router.invoke_method.assert_called_with(module_name='module2', method='remote_method', params=[], env={'variable1': 'value1'}, callback={})
-        assert_that(output.data, is_(json.dumps(['a', 'b', 'c'])))
+        assert_that(json.loads(output.data.decode(output.charset)), is_(['a', 'b', 'c']))
 
